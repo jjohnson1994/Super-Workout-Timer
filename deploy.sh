c2493e44b1008a7cd4f35110caed2a29985b5645
James Johnson <jamesjohnson@Jamess-MacBook-Pro.local>
2020-04-16 18:56:41 +0100

    add github pages deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:jjohnson1994/Super-Workout-Timer.git master:gh-pages
+
+cd -
